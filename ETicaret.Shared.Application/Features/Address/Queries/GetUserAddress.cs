cc0067ea68f7e769d129efc49131373bbf76da6e
Tanerbyk <tanerbuyuk06@gmail.com>
2023-03-25 23:52:55 +0300

    Create GetUserAddress.cs

---
+++ ETicaret.Shared.Application/Features/Address/Queries/GetUserAddress.cs
@@ -0,0 +1,14 @@
+﻿using ETicaret.Shared.Application.DTOs;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ETicaret.Shared.Application.Features.Address.Queries
+{
+    public class GetUserAddress : IRequest<AddressDto>
+    {
+    }
+}
