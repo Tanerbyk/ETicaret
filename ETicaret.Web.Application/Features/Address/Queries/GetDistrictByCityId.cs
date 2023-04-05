4f0f3bf6c02c56b4ad5fcff15861467605ce97d1
Tanerbyk <tanerbuyuk06@gmail.com>
2023-04-05 21:04:46 +0300

    Address&Account

---
+++ ETicaret.Web.Application/Features/Address/Queries/GetDistrictByCityId.cs
@@ -0,0 +1,34 @@
+﻿using ETicaret.Shared.Dal;
+using ETicaret.Shared.Dal.Concrete;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ETicaret.Shared.Application.Features.Address.Queries
+{
+    public class GetDistrictByCityId : IRequest<List<District>>
+    {
+        public int CityId { get; set; }
+
+        public class GetDistrictByCityIdHandler : IRequestHandler<GetDistrictByCityId, List<District>>
+        {
+            private readonly MarketPlaceDbContext _db;
+
+            public GetDistrictByCityIdHandler(MarketPlaceDbContext db)
+            {
+                _db = db;
+            }
+
+            public  Task<List<District>> Handle(GetDistrictByCityId request, CancellationToken cancellationToken)
+            {
+                var data = _db.Districts.Where(x=>x.CityId==request.CityId).ToListAsync();
+                return data;
+               
+            }
+        }
+    }
+}
---
+++ ETicaret.Web.Application/Features/Address/Queries/GetUserAddressQuery.cs
@@ -0,0 +1,59 @@
+﻿using ETicaret.Shared.Application.DTOs;
+using MediatR;
+using System.Security;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Claims;
+using ETicaret.Shared.Dal;
+using ETicaret.Shared.Dal.Web;
+
+namespace ETicaret.Shared.Application.Features.Address.Queries
+{
+    public class GetUserAddressQuery : IRequest<AddressDto>
+    {
+        public string userid { get; set; }
+        public class GetUserAddressQueryHandler : IRequestHandler<GetUserAddressQuery, AddressDto>
+        {
+            private readonly MarketPlaceDbContext _db;
+            private readonly UserManager<WebUser> _userManager;
+
+            public GetUserAddressQueryHandler(MarketPlaceDbContext db, UserManager<WebUser> userManager)
+            {
+                _db = db;
+                _userManager = userManager;
+            }
+
+            public async Task<AddressDto> Handle(GetUserAddressQuery request, CancellationToken cancellationToken)
+            {    
+                var userAddress = await _db.Addresses.FirstOrDefaultAsync(x => x.UserId == request.userid);
+                AddressDto ad = new();
+                ad.Cities = await _db.Cities.ToListAsync();
+                if (userAddress != null)
+                {
+                    ad.Districts = await _db.Districts.Where(x => x.CityId == userAddress.CityId).ToListAsync();
+                    ad.CityId = userAddress.CityId;
+                    ad.DistrictId = userAddress.DistrictId;
+                    ad.FullAddress = userAddress.FullAddress;
+                    ad.AddressTitle = userAddress.AddressTitle;
+                    ad.AdressDetail = userAddress.AddressDetail;
+
+                    //ViewBag.City = new SelectList(_db.Cities, "CityId", "Name");           
+                }
+                else
+                {
+                    ad.Districts = await _db.Districts.Where(x => x.CityId == 1).ToListAsync();
+                    ad.CityId = 0;
+                    ad.DistrictId = 0;
+                    ad.FullAddress = "";
+                    ad.AddressTitle = "";
+                }
+                return ad;
+            }
+        }
+    }
+}
