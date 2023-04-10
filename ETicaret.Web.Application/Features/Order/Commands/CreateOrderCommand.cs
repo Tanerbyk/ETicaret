6a8a6281065c49932325baceb3da1d7462750610
Tanerbyk <tanerbuyuk06@gmail.com>
2023-04-10 23:56:43 +0300

    Update CreateOrderCommand.cs

--- ETicaret.Web.Application/Features/Order/Commands/CreateOrderCommand.cs
+++ ETicaret.Web.Application/Features/Order/Commands/CreateOrderCommand.cs
@@ -1,4 +1,6 @@
-﻿using MediatR;
+﻿using ETicaret.Shared.Dal;
+using ETicaret.Shared.Dal.Concrete;
+using MediatR;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,8 +11,19 @@ namespace ETicaret.Web.Application.Features.Order.Commands
 {
     public class CreateOrderCommand :  IRequest<bool>
     {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public DateTime OrderDate { get; set; } = DateTime.Now;
+        public decimal TotalPrice { get; set; }
+        public List<OrderDetail> OrderDetails { get; set; }
         public class CreateOrderCommandHandler : IRequestHandler<CreateOrderCommand, bool>
         {
+            private readonly MarketPlaceDbContext _db;
+
+            public CreateOrderCommandHandler(MarketPlaceDbContext db)
+            {
+                _db = db;
+            }
 
             public Task<bool> Handle(CreateOrderCommand request, CancellationToken cancellationToken)
             {
