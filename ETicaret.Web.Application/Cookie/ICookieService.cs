17d62e3b59cc3a5f2067b8b7696d4e71ceb6aa61
Tanerbyk <tanerbuyuk06@gmail.com>
2023-08-07 22:19:16 +0300

    Singalr test

--- ETicaret.Web.Application/Cookie/ICookieService.cs
+++ ETicaret.Web.Application/Cookie/ICookieService.cs
@@ -10,11 +10,11 @@ namespace ETicaret.Web.Application.Cookie
 {
     public interface ICookieService
     {
-        public void SetCookie(string key, string value);
+         void SetCookie(string key, string value);
 
-        public string GetCookie(string key);
+         string GetCookie(string key);
 
-        public void RemoveCookie(string key);
+         void RemoveCookie(string key);
 
 
 
